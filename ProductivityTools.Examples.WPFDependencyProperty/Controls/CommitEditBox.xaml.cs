264b1d9b17d7e74968508310f8d469a371f37bee
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-01-28 20:16:12 +0100

    init comimt

---
+++ ProductivityTools.Examples.WPFDependencyProperty/Controls/CommitEditBox.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProductivityTools.Examples.WPFDependencyProperty.Controls
+{
+    /// <summary>
+    /// Interaction logic for CommitEditBox.xaml
+    /// </summary>
+    public partial class CommitEditBox : UserControl
+    {
+        public CommitEditBox()
+        {
+            InitializeComponent();
+        }
+
+        public string BoxText
+        {
+            get { return (string)GetValue(BoxTextProperty); }
+            set { SetValue(BoxTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty BoxTextProperty =
+            DependencyProperty.Register("BoxText", typeof(string), typeof(CommitEditBox), new PropertyMetadata(string.Empty,OnBoxTextChanged));
+
+        private static void OnBoxTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            CommitEditBox notesControl = d as CommitEditBox;
+            notesControl.OnSetTextChanged(e);
+        }
+
+        private void OnSetTextChanged(DependencyPropertyChangedEventArgs e)
+        {
+            txt.Text = e.NewValue?.ToString();
+        }
+    }
+}
